a3d834d8d8fe7de6b41463ce25da618a53a1d5d2
Khalid759 <khalid.ali@kraftshala.com>
2024-12-10 15:16:15 +0530

    resovle error

--- components/HomeComponents/machineDescriptoin.js
+++ components/HomeComponents/machineDescriptoin.js
@@ -17,10 +17,10 @@ export default function MachineDescription() {
         <div className="machinDescription-heading">Description produit</div>
         <div className="machinDescription-desc">
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-          been the industry's standard dummy text ever since the 1500s, when an unknown printer took
-          a galley of type and scrambled it to make atype specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting, remaining essentially
-          unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+          been the industry standard dummy text ever since the 1500s, when an unknown printer took a
+          galley of type and scrambled it to make atype specimen book. It has survived not only five
+          centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+          It was popularised in the 1960s with the release of Letraset sheets containing
         </div>
       </div>
       <div className="description-todo">
